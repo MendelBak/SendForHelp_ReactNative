970c4e7b1eb7300fd3b09802699e07ed0abf16f4
MB <mendelbak@gmail.com>
2021-01-27 23:22:48 +0200

    First commit

---
+++ SendForHelp/App.tsx
@@ -0,0 +1,71 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {View, Button, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const Stack = createStackNavigator();
+
+// You can import Ionicons from @expo/vector-icons/Ionicons if you use Expo or
+// react-native-vector-icons/Ionicons otherwise.
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = 'information-circle'
+
+            // if (route.name === 'Home') {
+            //   iconName = focused
+            //     ? 'information-circle'
+            //     : 'information-circle-outline';
+            // } else if (route.name === 'Settings') {
+            //   iconName = focused ? 'ios-arrow-dropdown' : 'ios-arrow-dropdown';
+            // }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} title='Delete this title'/>;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
---
+++ SendForHelp/Readme.md
@@ -0,0 +1,4 @@
+To run: 
+`npm install`
+`npx react-native start`
+`npx react-native run-android`
