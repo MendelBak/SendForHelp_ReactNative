d51bcf72f3f96e58b85fde7bf5808b90f7e503b7
MB <mendelbak@gmail.com>
2021-03-12 16:31:44 +0200

    Created new user model

---
+++ SendForHelp/src/models/user.model.ts
@@ -0,0 +1,28 @@
+import {makeAutoObservable} from 'mobx';
+import EmergencyLocationModel from './emergencyLocation.model';
+import SymptomModel from './symptom.model';
+
+export default class UserModel {
+  _id?: string;
+  firstName!: string;
+  middleName?: string;
+  lastName!: string;
+  preferredName?: string;
+  age!: number;
+  email!: string;
+  googleId!: string;
+
+  constructor(userModel?: UserModel) {
+    makeAutoObservable(this);
+    if (userModel) {
+      this._id = userModel?._id;
+      this.firstName = userModel.firstName;
+      this.middleName = userModel.middleName;
+      this.lastName = userModel.lastName;
+      this.preferredName = userModel.preferredName;
+      this.age = userModel.age;
+      this.email = userModel.email;
+      this.googleId = userModel.googleId;
+    }
+  }
+}
