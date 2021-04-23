49a60f0588e67e6c97bb89c4767db20d0bece15b
MB <mendelbak@gmail.com>
2021-04-23 13:51:09 +0300

    Added icons to bottom tab navigation

--- SendForHelp/App.tsx
+++ SendForHelp/App.tsx
@@ -21,6 +21,7 @@ import LocationDetailsScreen from './src/screens/LocationDetails';
 import PushNotification from 'react-native-push-notification';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // TODO: Need to move these out of here. General cleanup of notifications is needed.
 // Function for Local Notification
@@ -55,40 +56,83 @@ PushNotification.configure({
 });
 
 export default function App() {
-  // TODO: This should be in a store. (user.store.ts?) Need to make user.mode.ts anyway.
+  // TODO: This should be in a store. (user.store.ts?) Need to make user.model.ts anyway.
   const isSignedIn: boolean = true;
 
   const TabNavigator = () => {
     return (
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName = 'information-circle';
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
+          // tabBarIcon: ({ focused, color, size }) => {
+          //   let iconName = 'information-circle';
+          //   // You can return any component that you like here!
+          //   return <Ionicons name={iconName} size={size} color={color} />;
+          // },
         })}
         tabBarOptions={{
-          activeTintColor: 'tomato',
+          activeTintColor: 'red',
           inactiveTintColor: 'gray',
         }}>
         {isSignedIn ? (
           <>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Symptoms" component={SymptomsScreen} />
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName = 'home';
+
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+              }}
+            />
+            <Tab.Screen
+              name="Symptoms"
+              component={SymptomsScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName = 'details';
+
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+              }}
+            />
             <Tab.Screen
               name="Location Details"
               component={LocationDetailsScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName = 'document-text-outline';
+
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+              }}
             />
             <Tab.Screen
               name="First Responder"
               component={FirstResponderScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName = 'people-outline';
+
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+              }}
             />
           </>
         ) : (
           <>
-            <Tab.Screen name="Login" component={LoginScreen} />
+            <Tab.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName = 'login';
+
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+              }}
+            />
             <Tab.Screen name="Signup" component={SignupScreen} />
           </>
         )}
@@ -99,7 +143,7 @@ export default function App() {
   return (
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator>
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        <Stack.Screen name="Send For Help" component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
